08875c36a8c8960b6a1a18098f7aece2aeb20c38
MarvinFrisch <marvin.frisch@kit.edu>
2023-10-30 15:08:52 +0000

    Restructured config and code Restructured config to be object based Restructured code to call functions instead of giant init

--- src/processing_node/processing_node/processing_node.py
+++ src/processing_node/processing_node/processing_node.py
@@ -3,6 +3,8 @@ import rclpy
 from rclpy.node import Node
 from ros2topic.api import get_msg_class
 
+import sys
+import yaml
 import importlib
 
 from .processing_function_handler import processing_function
@@ -12,45 +14,114 @@ from std_msgs.msg import Float32
 class ProcessingNode(Node):
 
     def __init__(self):
-        # Construct Node, automatically discover parameters from yaml file
-        super().__init__('ProcessingNode', automatically_declare_parameters_from_overrides = True)
+        super().__init__('ProcessingNode')
 
-        self.get_logger().info("Loading parameters from yaml")
+        config_path = sys.argv[1]
 
-        # Get dict of inputs from parameters
-        self.input_dict = self.get_parameters_by_prefix('Inputs')
+        config = self.loadConfig(config_path)
 
-        # Load the actual mappings of input names
-        for key in self.input_dict.keys():
-            self.input_dict[key] = self.get_parameter(f'Inputs.{key}').get_parameter_value().string_array_value
+        # Create data dict which carries the input data
+        self.data_dict = dict.fromkeys(config['Inputs'].keys(), [])
 
-        # Create data dict with the same keys as the input_dict
-        self.data_dict = dict.fromkeys(self.input_dict.keys(), [])
-
-        # Create sample number dict
-        self.sample_number_dict = self.get_parameters_by_prefix('NumberOfInputSamples')
-
-        # Load the number of samples per input name
-        for key in self.sample_number_dict.keys():
-            self.sample_number_dict[key] = self.get_parameter(f'NumberOfInputSamples.{key}').get_parameter_value().integer_value
-        
-        # Import custom message types specified in yaml
-        # Not sure if needed right now, kept for future proofing (possibly for sending output?)
-        try:
-            import_dict = self.get_parameters_by_prefix('Extras.Imports')
-            for to_import in import_dict.keys():
-                import_list = self.get_parameter(f'Extras.Imports.{to_import}').get_parameter_value().string_array_value
-                importlib.import_module(import_list[0], import_list[1])
-        except:
-            pass
+        self.importNeededModules(config)
 
         self.get_logger().info("Setting up subscriptions")
 
-        # Create subscription to every topic specified in parameters
-        for input_name in self.input_dict.keys():
+        topic_dict = self.mapInputNamesToTopics(config)
+
+        self.setUpSubscriptions(topic_dict, config)
+
+        # Set up publisher for the model output
+        self.output_publisher = self.create_publisher(Float32, 'loss', 10) 
+        # TODO: Make output publisher customizable with config input
+
+        self.get_logger().info("Starting the main processing loop")
+
+        # Get model path from config
+        self.model_path = config['ModelInformation']['ModelPath']
+
+        # Get model inference timer period from config
+        inference_timer_period = float(config['ModelInformation']['ModelInferenceTimerPeriod'])
+
+        # Create timer that calls the processing function based on a timer period specified in the config
+        self.processing_timer = self.create_timer(inference_timer_period, self.timer_callback)
+    
+    def loadConfig(self, config_path):
+        """
+        Loads config from path and returns it as a dict
+
+        Args:
+            config_path (String): Absolute path to the config file expected to be .yaml
+        
+        Returns:
+            config dict
+        """
+
+        try:
+            # Get dict of inputs from parameters
+            with open(config_path, 'r') as file:
+                config = yaml.safe_load(file)
+            return config
+
+        except:
+            self.get_logger().error("Please specify valid config path")
+            rclpy.shutdown()
+            return
+        
+    def mapInputNamesToTopics(self, config):
+        """
+        Load the actual mappings of input names
+
+        Args:
+            config (dict): Config dict that specifies requested inputs and relevant information about those inputs
+        
+        Returns:
+            topic dict specifying which topics to subscribe to and what fields of those topics are carrying which input information
+        """
+
+        topic_dict = {}
+
+        for key in config['Inputs']:
+            topic = config['Inputs'][key]['topic'][0]
+            field = ''
+            for count, sub in enumerate(config['Inputs'][key]['topic'][1:]):
+                if 0 < count < len(config['Inputs'][key]['topic'])-1:
+                    field += '.'
+                field += sub
+            if topic not in topic_dict:
+                topic_dict[topic] = {key: field}
+            else:
+                topic_dict[topic][key] = field
+        
+        return topic_dict
+    
+    def importNeededModules(self, config):
+        """
+        Imports Modules specified in config dict, possibly needed for output message types
+
+        Args:
+            config (dict): Config dict that specifies which modules to import from where
+        """
+
+        try:
+            import_dict = config['Extras']['Imports']
+            for to_import in import_dict.keys():
+                import_list = import_dict[to_import]
+                importlib.import_module(import_list[0], import_list[1])
+        except:
+            pass
+    
+    def setUpSubscriptions(self, topic_dict, config):
+        """
+        Sets up Subscriptions to topics that are mentioned in topic_dict, also sets up listener_callback with correct input-message field mapping
+
+        Args:
+            topic_dict (dict): dict that contains the topics to be subscribed to, the inputs they carry and in which field these inputs are
+            config (dict): dict that contains more information about the inputs
+        """
+
+        for topic_name in topic_dict.keys():
             subscribed = False
-            topic_name = self.input_dict[input_name][0]
-            field_name = self.input_dict[input_name][1]
             while not subscribed:
                 # Get all currently publishing topics
                 topic_list = self.get_topic_names_and_types()
@@ -60,57 +131,44 @@ class ProcessingNode(Node):
                         # Get msg type 
                         msg_type = get_msg_class(self, topic_tuple[0], include_hidden_topics=True)
                         # Set up subscription
-                        self.create_subscription(msg_type, topic_tuple[0], lambda msg, field_name=field_name, dict_key=input_name : self.listener_callback(msg, field_name, dict_key), 10)
+                        self.create_subscription(msg_type, topic_tuple[0], lambda msg, field_names=topic_dict[topic_name] : self.listener_callback(msg, field_names), 10)
                         # Toggle bool
                         subscribed = True
-                        self.get_logger().info(f"Subscribed to topic {topic_name} which publishes {input_name}")
+                        self.get_logger().info(f"Subscribed to topic {topic_name} which publishes {[config['Inputs'][i]['Name'] for i in topic_dict[topic_name]]}")
 
-        # Set up publisher for the model output
-        self.output_publisher = self.create_publisher(Float32, 'loss', 10) 
-        # TODO: Make output publisher customizable with config input
+    def listener_callback(self, msg, field_names):
+        """
+        Function that is called whenever something is published to a subscribed topic and modifies the data dict accordingly
 
-        self.get_logger().info("Starting the main processing loop")
+        Args:
+            msg (ROS2 Message): Message that triggered the function call
+            field_names (dict): Dict that specifies which fields are carrying what input information
+        """
 
-        # Get model path from config
-        self.model_path = self.get_parameter('ModelInformation.ModelPath').get_parameter_value().string_value
-
-        # Get model inference timer period from config
-        inference_timer_period = self.get_parameter('ModelInformation.ModelInferenceTimerPeriod').get_parameter_value().double_value
-
-        # Create timer that calls the processing function based on a timer period specified in the config
-        self.processing_timer = self.create_timer(inference_timer_period, self.timer_callback)
-    
-    # One-fits-all-function that takes messages and edits the data dict accordingly
-    def listener_callback(self, msg, field_name, dict_key):
-        # Sanity check if the input name is requested by the config
-        if dict_key in self.sample_number_dict:
-            # Read the relevant message field and append it to the relevant list in the data dict 
-            self.data_dict[dict_key].append(getattr(msg, field_name))
-            # Calculate maximum number of samples in the dict
-            maximum_sample_number = self.sample_number_dict[dict_key]
-            # Ensure that no more than the latest number of samples are in the dict
-            if len(self.data_dict[dict_key]) > maximum_sample_number:
-                self.data_dict[dict_key].pop(0)
+        # Read the relevant message field and append it to the relevant list in the data dict
+        for input in self.data_dict.keys():
+            # Does not work with append for whatever reason
+            self.data_dict[input] = self.data_dict[input] + [getattr(msg, field_names[input])]
             
     def timer_callback(self):
-        # Inference Mode
-        # Check if there is enough data in the data dict to run an inference
-        number_of_features_ready = 0
-        for key in self.sample_number_dict.keys():
-            if not len(self.data_dict[key]) == self.sample_number_dict[key]:
-                self.get_logger().info("Waiting for data")
-            else:
-                number_of_features_ready += 1
+        """
+        Function that is called on a timer and sends collected input data to the processing function aswell as publish the resulting output
+        """
 
-        if number_of_features_ready == len(self.sample_number_dict.keys()):
-            #  Call processing function
-            processed_data = processing_function(self.data_dict, self.model_path)
+        #  Call processing function
+        processed_data = processing_function(self.data_dict, self.model_path)
 
-            # Publish output
-            # TODO: Make output customizable
+        # Reset data dict
+        self.data_dict = dict.fromkeys(self.data_dict.keys(), [])
+
+        # Publish output
+        # TODO: Make output customizable
+        try:
             output_msg = Float32()
             output_msg.data = float(processed_data)
             self.output_publisher.publish(output_msg)
+        except:
+            pass
 
 
 def main(args=None):
