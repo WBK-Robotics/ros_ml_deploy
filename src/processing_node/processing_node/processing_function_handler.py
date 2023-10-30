2b4907347db8b73b90f933741a0347a085f8ad5c
MarvinFrisch <marvin.frisch@kit.edu>
2023-10-30 15:11:23 +0000

    Updated processing to work with restructured node Mainly used for testing purposes

--- src/processing_node/processing_node/processing_function_handler.py
+++ src/processing_node/processing_node/processing_function_handler.py
@@ -5,20 +5,26 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+# Create one list to input into the model
+input_list = []
+
 # Main processing function calling a model with given inputs and returning a dict as output
 def processing_function(input_dict: dict, model_path: str) -> dict:
-    # Create one list to input into the model
-    input_list = []
+    global input_list
     # Fill the input list with data 
     for key in input_dict.keys():
         input_list += input_dict[key]
     
+    if len(input_list) < 100:
+        return
+
+    if len(input_list) > 100:
+        input_list = input_list[len(input_list)-100:]
+
     # Scale Data
     max_input = max(input_list)
     min_input = min(input_list)
     input_list = [(element - min_input) / (max_input - min_input) for element in input_list]
-
-    # TODO: Generalize scaling for multiple keys
 
     input_array = np.array(input_list, dtype=np.float32)
     input_array = np.reshape(input_array, (1, -1))
@@ -35,6 +41,6 @@ def processing_function(input_dict: dict, model_path: str) -> dict:
     loss = np.mean((recon-input_array)**2, axis=2)
 
     loss = loss[0][0]
-
+    
     return loss
 
