39d689df95c025a43ecf5af95679b73b70399931
liquidcronos <jan-baumgaertner@web.de>
2023-11-29 09:08:35 +0100

    added test for config validation

--- src/processing_node/test/test_processing_node.py
+++ src/processing_node/test/test_processing_node.py
@@ -1,9 +1,62 @@
 import pytest
 import yaml
-from processing_node.processing_node import ProcessingNode
+from processing_node.processing_node import ProcessingNode, check_if_config_is_valid
 import rclpy
 import os
 import ament_index_python.packages
+
+
+
+valid_config = {
+    "Inputs": {
+        "Motor Current 1": {
+            "Description": "Example input parameter, for instance emulating a motor current",
+            "Topic": "sensor_data",
+            "Field": ["data"]
+        }
+    },
+    "Outputs": {
+        "float parameter": {
+            "Description": "Example float parameter",
+            "Topic": "ExampleFloat",
+            "Field": ["data"],
+            "MessageType": "Float32"
+        },
+        "int parameter": {
+            "Description": "Example int parameter",
+            "Topic": "ExampleInt",
+            "Field": ["data"],
+            "MessageType": "Int16"
+        }
+    },
+    "Imports": {
+        "Float32": {
+            "Package": "std_msgs.msg",
+            "Module": "Float32"
+        },
+        "Int16": {
+            "Package": "std_msgs.msg",
+            "Module": "Int16"
+        }
+    }
+}
+
+
+invalid_config = {
+    "Inputs": {
+        "Motor Current 1": {
+            "Description": "Missing Topic and Field",
+        }
+    },
+    "Outputs": {
+        "float parameter": {
+            "Description": "Missing Topic, Field, and MessageType",
+        }
+    },
+
+}
+
+
 
 
 def get_config_file_path(filename):
@@ -15,54 +68,30 @@ def get_config_file_path(filename):
 
     return config_file_path
 
+def test_config_validation():
+    # Arrange
+    # Act
+    # Assert
+    assert check_if_config_is_valid(valid_config)[0] == True
+    assert check_if_config_is_valid(invalid_config)[0] == False
 
-def test_valid_configuration_file():
+
+
+def test_config_loading():
     rclpy.init()
     config_path = get_config_file_path("config.yaml")
     # Arrange
-    config_dict = {
-        "Inputs": {
-            "Motor Current 1": {
-                "Description": "Example input parameter, for instance emulating a motor current",
-                "Topic": "sensor_data",
-                "Field": ["data"]
-            }
-        },
-        "Outputs": {
-            "float parameter": {
-                "Description": "Example float parameter",
-                "Topic": "ExampleFloat",
-                "Field": ["data"],
-                "MessageType": "Float32"
-            },
-            "int parameter": {
-                "Description": "Example int parameter",
-                "Topic": "ExampleInt",
-                "Field": ["data"],
-                "MessageType": "Int16"
-            }
-        },
-        "Imports": {
-            "Float32": {
-                "Package": "std_msgs.msg",
-                "Module": "Float32"
-            },
-            "Int16": {
-                "Package": "std_msgs.msg",
-                "Module": "Int16"
-            }
-        }
-    }
+    
 
     with open(config_path, "w") as file:
-        yaml.dump(config_dict, file)
+        yaml.dump(valid_config, file)
     
     # Act
     node = ProcessingNode(lambda x: x,config_path=config_path)  # Dummy function
     loaded_config = node.load_config(str(config_path))
     
     # Assert
-    assert loaded_config == config_dict
+    assert loaded_config == valid_config
 
 
 if __name__ == '__main__':
