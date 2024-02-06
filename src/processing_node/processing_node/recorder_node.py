5b2a292483f2a31c707bdf3d8b55033b5ae35d2d
Marvin Frisch <marvin.frisch@kit.edu>
2024-02-06 12:14:55 +0000

    Added parameter "number_of_input_points" to set how many data points to record

--- src/processing_node/processing_node/recorder_node.py
+++ src/processing_node/processing_node/recorder_node.py
@@ -1,28 +1,38 @@
 import rclpy
 import csv
-import sys
 
+from asyncio import Future
 from rclpy.node import Node
 
 from processing_node.processing_node import ProcessingNode
 from processing_node.ml_deploy_library import *
 
+recording_done = Future()
+
 class Recorder:
 
     def __init__(self):
-        pass
+        self.parameters = {"number_of_input_points" : int}
     
-    def set_parameters(self):
-        # TODO: Possibly set wanted amount of data here?
-        pass
+    def set_parameters(self, parameters:dict):
+        for key in parameters.keys():
+            if key in self.parameters.keys():
+                self.parameters[key] = parameters[key]
+            else:
+                raise ValueError("The parameter {} is not supported by this processor".format(key))
     
     def get_parameters(self):
-        pass
+        return self.parameters
 
     def execute(self, aggregated_data_dict):
-        with open('/root/mounted_folder/Files/testfile.csv', 'w') as f:
-            w = csv.writer(f)
-            w.writerows(aggregated_data_dict.items())
+        number_of_input_points = len(max(aggregated_data_dict.values(), key=len))
+        print(number_of_input_points)
+        if number_of_input_points >= self.parameters["number_of_input_points"]:
+            with open('/root/mounted_folder/Files/testfile.csv', 'w') as f:
+                w = csv.writer(f)
+                w.writerows(aggregated_data_dict.items())
+            recording_done.set_result("Done")
+        
 
 def main():
     rclpy.init(args=None)
@@ -32,8 +42,7 @@ def main():
     if 'Outputs' in config:
         config.pop('Outputs')
     
-    processsing_node = ProcessingNode(recorder_object, config=config,frequency=1)
+    recorder_node = ProcessingNode(recorder_object, config=config,frequency=200)
+    recorder_object.set_parameters({"number_of_input_points":5})
 
-    rclpy.spin(processsing_node)
-
-# TODO: Manual config loading, removing outputs from config, setting up function with csv export
+    rclpy.spin_until_future_complete(recorder_node, recording_done)
