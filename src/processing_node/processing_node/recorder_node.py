66604f224b129b54fb985786091db296d11600fb
Marvin Frisch <marvin.frisch@kit.edu>
2024-02-07 15:02:15 +0000

    Changed setup of parameters and output Now expects a StringIO object as output format

--- src/processing_node/processing_node/recorder_node.py
+++ src/processing_node/processing_node/recorder_node.py
@@ -3,6 +3,7 @@ import csv
 import sys
 import ament_index_python
 
+from io import StringIO
 from asyncio import Future
 
 from processing_node.processing_node import ProcessingNode
@@ -19,8 +20,8 @@ def get_config_file_path(filename):
 
 class RecorderNode(ProcessingNode):
 
-    def __init__(self, config_path:str=None, frequency: float=30.0):
-        rclpy.Node.__init__(self, "recorder_node")
+    def __init__(self, config_path:str=None, outfile:StringIO=None, frequency: float=30.0):
+        rclpy.node.Node.__init__(self, "recorder_node")
 
         self._config = load_config(config_path)
 
@@ -28,13 +29,15 @@ class RecorderNode(ProcessingNode):
         if not config_is_valid:
             raise ValueError(error_message)
         
+        self.outfile = outfile
+
         self.supported_message_types = import_needed_modules(self._config)
         self.aggregated_input_data = dict.fromkeys(self._config['Inputs'].keys(), [])
 
         input_topic_dict = map_input_and_output_names_to_topics(self._config)[0]
         self.set_up_subscriptions(input_topic_dict)
 
-        self.parameters = {"number_of_input_points" : int, "path_to_csv": str}
+        self.declare_parameter("number_of_input_points", 1000)
 
         # Set model timer period
         timer_period = 1.0 / frequency
@@ -42,30 +45,12 @@ class RecorderNode(ProcessingNode):
         self.timer = self.create_timer(timer_period, self.execute)
 
         self.recording_done = Future()
-    
-    def set_parameters(self):
-        for param_name, param_type  in self.parameters.items():
-            if self.has_parameter(param_name):
-                self.get_logger().warn(f"Parameter {param_name} is already declared.")
-                continue
 
-            default_values = {int: 0,
-                              float: 0.0,
-                              str: "",
-                              bool: False}
-
-            if param_type not in default_values:
-                self.get_logger().error(f"Parameter type {param_type} is not supported.")
-                continue
-
-            self.declare_parameter(param_name, default_values[param_type])
-
-    def execute(self, aggregated_data_dict):
-        number_of_input_points = len(max(aggregated_data_dict.values(), key=len))
-        if number_of_input_points >= self.parameters["number_of_input_points"]:
-            with open(self.parameters['path_to_csv'], 'w') as f:
-                w = csv.writer(f)
-                w.writerows(aggregated_data_dict.items())
+    def execute(self):
+        number_of_input_points = len(max(self.aggregated_input_data.values(), key=len))
+        if number_of_input_points >= self.get_parameter("number_of_input_points").value:
+            w = csv.writer(self.outfile)
+            w.writerows(self.aggregated_input_data.items())
             self.recording_done.set_result("Done")
         
 
@@ -81,8 +66,7 @@ def main():
 
     config_path = get_config_file_path('config.yaml')
 
-    recorder_node = RecorderNode(config_path,frequency=200)
+    with open(path_to_csv, 'w') as csv_out_file:
+        recorder_node = RecorderNode(config_path, csv_out_file, frequency=200)
 
-    recorder_node.set_parameters({"number_of_input_points" : 10, "path_to_csv": path_to_csv})
-
-    rclpy.spin_until_future_complete(recorder_node, recorder_node.recording_done)
+        rclpy.spin_until_future_complete(recorder_node, recorder_node.recording_done)
