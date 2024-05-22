55a7e6e506600b1ab70b0b2f94bf22d7eabafb33
Marvin Frisch <marvin.frisch@kit.edu>
2024-05-22 06:26:23 +0000

    Adjusted version number in docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'ROSMLDeploy'
 copyright = '2024, Marvin Frisch, Jan Baumgärtner'
 author = 'Marvin Frisch, Jan Baumgärtner'
-release = '1.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
